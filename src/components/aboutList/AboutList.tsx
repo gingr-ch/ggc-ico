69fb6e1ea8a1224d240ccb9507d6991614693d01
jeff <jeffstahlin@gmail.com>
2018-06-25 14:56:45 +0200

    add What is Gingr section

--- src/components/aboutList/AboutList.tsx
+++ src/components/aboutList/AboutList.tsx
@@ -3,19 +3,19 @@ import * as React from 'react';
 import './AboutList.css';
 
 class AboutList extends React.Component {
-  render () {
+  render() {
     const items = [
       { id: 1, name: 'Client', text: 'download' },
       { id: 2, name: 'Platform', anchor: 'platform' },
       { id: 3, name: 'GG Coin', anchor: 'ggcoin' }
     ];
-    const itemsList = items.map((item) =>
-      <li key={item.id.toString()}><a href="#{item.anchor}">{item.name}</a></li>
-    );
+    const itemsList = items.map(item => (
+      <li key={item.id.toString()}>
+        <a href="#{item.anchor}">{item.name}</a>
+      </li>
+    ));
 
-    return (
-      <div>list</div>
-    );
+    return <ul>{{ itemsList }}</ul>;
   }
 }
 
--- src/containers/whatIsGingr/WhatIsGingr.tsx
+++ src/containers/whatIsGingr/WhatIsGingr.tsx
@@ -1,14 +1,102 @@
 import * as React from 'react';
+import ReactPlayer from 'react-player';
 
 import './WhatIsGingr.css';
 
+const clientIcon = require('../../assets/img/icons/client.png');
+const gingrIcon = require('../../assets/img/icons/gingr.png');
+const agencyIcon = require('../../assets/img/icons/agency.png');
+const establishmentIcon = require('../../assets/img/icons/establishment.png');
+const gingrImg = require('../../assets/img/what_is_gingr--gingr.png');
+const establishmentImg = require('../../assets/img/what_is_gingr--establishment.png');
+
+const whatIsList = [
+  {
+    id: 0,
+    title: 'Client',
+    classTitle: 'client',
+    icon: clientIcon,
+    content:
+      'Worldwide access to instant and guaranteed sex in a quick and effortless way. Have their information stored while remaining anonymous and pay securely, fast and confidentially',
+    video: 'https://vimeo.com/194665559'
+  },
+  {
+    id: 1,
+    title: 'Gingr',
+    subtitle: 'Sex-Worker',
+    classTitle: 'gingr',
+    icon: gingrIcon,
+    content:
+      'Worldwide work with only one tool which can connect to millions of clients. Recruitment and workspaces provided by Gingr Connect and Gingr Pop n Go. Receive payments directly on our platform from pre-paying clients with GG Coin. Feel safe every step of the journey with Gingr Care.',
+    img: gingrImg
+  },
+  {
+    id: 2,
+    title: 'Agency & Establishment',
+    classTitle: 'agency-establishment',
+    icon: establishmentIcon,
+    icon2: agencyIcon,
+    content:
+      'State of the art management features to assist with bookings, personnel requirements and many other areas of the business. Geo-location and advertising on a site that everybody is talking about. Access to pre-paying clients and accounts to store funds from sale revenues with Gingr Bank.',
+    img: establishmentImg
+  }
+];
 class WhatIsGingr extends React.Component {
-  render () {
+  player: any;
+
+  ref = (player: any) => {
+    this.player = player;
+  };
+
+  render() {
     return (
       <div className="gg-section-container gg-section-container--what-is-gingr">
         <div className="gg-content-container">
           <h1>What is Gingr?</h1>
-          <h3>"THE FIRST GLOBAL MARKET PLACE WITH CRYPTO PAYMENTS FOR THE PAID SEX INDUSTRY."</h3>
+          <h3>
+            "THE FIRST GLOBAL MARKET PLACE WITH CRYPTO PAYMENTS FOR THE PAID SEX
+            INDUSTRY."
+          </h3>
+
+          <div className="list-2-col">
+            {whatIsList.map((d, i) => (
+              <div key={d.id} className="l2c__container">
+                <div className="l2c__media">
+                  {d.img && (
+                    <img className="l2c__media-image" src={d.img} alt="" />
+                  )}
+                  {d.video && (
+                    <ReactPlayer
+                      url={d.video}
+                      width="431px"
+                      height="232px"
+                      playing={true}
+                      muted={true}
+                      controls={false}
+                      loop={false}
+                      ref={this.ref}
+                      className="l2c__media-image"
+                    />
+                  )}
+                </div>
+                <div className={'l2c__content l2c__content--' + d.classTitle}>
+                  <div className="l2c__content-title">
+                    {d.icon2 && <img src={d.icon2} alt="" />}
+                    <img src={d.icon} alt="" />
+                    <h3>
+                      {d.title}
+                      {d.subtitle && (
+                        <span className="l2c__content-subtitle">
+                          ({d.subtitle})
+                        </span>
+                      )}
+                    </h3>
+                  </div>
+                  <p>{d.content}</p>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     );
