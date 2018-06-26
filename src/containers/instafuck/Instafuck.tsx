95b536d199a02c65c3165a5315814897e7a9d8af
jeff <jeffstahlin@gmail.com>
2018-06-26 16:04:27 +0200

    add Instafuck sections

---
+++ src/containers/instafuck/Instafuck.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+// import OnVisible from 'react-on-visible';
+
+import './Instafuck.css';
+
+class Instafuck extends React.Component {
+  render() {
+    return (
+      <div className="gg-section-container gg-section-container--instafuck">
+        <div className="gg-content-container">
+          <h3>Welcome to</h3>
+          <h1>Instafuck</h1>
+          <h4>
+            "We have created the world’s first button where you get instant
+            access with a guaranteed booking for sex"
+          </h4>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Instafuck;
