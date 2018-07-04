7fb96fa27b809218ebc5052ea553b9c3485e5b43
Unknown <jeffstahlin@gmail.com>
2018-07-04 16:24:29 +0200

    add Contact Us section

---
+++ src/containers/contactUs/ContactUs.tsx
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import Button from '../../components/button/Button';
+
+import './ContactUs.css';
+
+// const telegram = require('../../assets/img/social/telegram.png');
+
+class ContactUs extends React.Component {
+  click() {
+    window.open('https://admin.ggcico.io/auth/register', '_blank');
+  }
+
+  render() {
+    return (
+      <div className="gg-section-container gg-section-container--contactus">
+        <div
+          className="gg-content-container flex-row"
+          style={{ justifyContent: 'flex-start' }}
+        >
+          <h1>Contact Us</h1>
+
+          <div
+            className="flex-column"
+            style={{
+              alignItems: 'stretch',
+              position: 'relative',
+              width: '563px',
+            }}
+          >
+            <input
+              className="contactus-input"
+              type="text"
+              placeholder="Email"
+            />
+            <textarea className="contactus-textarea" placeholder="Message" />
+            <Button click={this.click}>Send</Button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ContactUs;
