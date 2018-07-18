b22f117df06e9755b97c823dd7f47fe693a1c38d
Unknown <jeffstahlin@gmail.com>
2018-07-18 16:10:10 +0200

    start Top mobile layout

--- src/containers/top/Top.tsx
+++ src/containers/top/Top.tsx
@@ -52,44 +52,55 @@ class Top extends React.Component {
     return (
       <div className="gg-section-container">
         <div className={classes}>
-          <h1 style={{ fontSize: '36px' }}>
-            "REVOLUTIONIZING<br /> THE OLDEST INDUSTRY <br />IN THE WORLD"
-          </h1>
-          <div style={{ display: 'flex', flexDirection: 'column' }}>
-            <div style={{ fontSize: '24px', marginBottom: '16px' }}>
-              PRIVATE SALE NOW
-            </div>
-            <div className="color-green" style={{ fontSize: '30px' }}>
-              70% DISCOUNT
-            </div>
+          <div className="gg-top-left__container">
+            <h1 style={{ fontSize: '36px' }}>
+              "REVOLUTIONIZING<br /> THE OLDEST INDUSTRY <br />IN THE WORLD"
+            </h1>
+            <div className="spacer" />
             <div
-              className="color-green"
-              style={{ fontSize: '27px', marginBottom: '16px' }}
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+              }}
             >
-              1 GGC = 0.03 EUR
+              <div style={{ fontSize: '24px', marginBottom: '16px' }}>
+                PRIVATE SALE NOW
+              </div>
+              <div className="color-green" style={{ fontSize: '30px' }}>
+                70% DISCOUNT
+              </div>
+              <div
+                className="color-green"
+                style={{ fontSize: '27px', marginBottom: '16px' }}
+              >
+                1 GGC = 0.03 EUR
+              </div>
+              <Button click={this.click} borderColor="#00D646">
+                Buy coins now
+              </Button>
             </div>
-            <Button click={this.click} borderColor="#00D646">
-              Buy coins now
-            </Button>
-          </div>
-          <img
-            src={chart}
-            alt="Chart"
-            style={{ maxWidth: '600px', width: '100%' }}
-          />
-          <div
-            style={{
-              position: 'absolute',
-              zIndex: 9,
-              width: '122px',
-              height: '122px',
-              right: '-61px',
-              top: 'calc(50% - 61px)',
-            }}
-          >
-            <OnVisible className="rotate-in">
-              <ButtonPlay videoToggle={this.videoToggle}>PLAY</ButtonPlay>
-            </OnVisible>
+            <div className="spacer" />
+            <div className="spacer" />
+            <img
+              src={chart}
+              alt="Chart"
+              style={{ maxWidth: '600px', width: '100%' }}
+            />
+            <div
+              style={{
+                position: 'absolute',
+                zIndex: 9,
+                width: '122px',
+                height: '122px',
+                right: '-61px',
+                top: 'calc(50% - 61px)',
+              }}
+            >
+              <OnVisible className="rotate-in">
+                <ButtonPlay videoToggle={this.videoToggle}>PLAY</ButtonPlay>
+              </OnVisible>
+            </div>
           </div>
         </div>
         <div style={{ flex: '1 1 50%', background: 'transparent' }}>
