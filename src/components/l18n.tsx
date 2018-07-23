7804daeafbc44f98c4a843b65988bab2a9ee900b
Unknown <jeffstahlin@gmail.com>
2018-07-23 16:47:14 +0200

    add shared l18n file for type definitions

---
+++ src/components/l18n.tsx
@@ -0,0 +1,73 @@
+import { LocalizedStringsMethods } from 'react-localization';
+
+export interface TopStrings extends LocalizedStringsMethods {
+  revolutionizing: string;
+  theOldestIndustry: string;
+  inTheWorld: string;
+  play: string;
+  privateSale: string;
+  discount: string;
+  buyCoinsNow: string;
+}
+
+export interface InstafuckStrings extends LocalizedStringsMethods {
+  welcomeTo: string;
+  instafuck: string;
+  weHaveCreatedThe: string;
+  instafuckStep1: string;
+  instafuckStep2: string;
+  instafuckStep3: string;
+  instafuckStep4: string;
+  instafuckStep5: string;
+  clientPhone: string;
+  gingrPhone: string;
+}
+
+export interface BetaStrings extends LocalizedStringsMethods {
+  gingr: string;
+  beta: string;
+  comingSoon: string;
+  clickHereToVisit: string;
+}
+
+export interface DownloadStrings extends LocalizedStringsMethods {
+  download: string;
+  whitepaper: string;
+  lightpaper: string;
+  numbers: string;
+  companysMemorandum: string;
+}
+
+export interface WhatIsGingrStrings extends LocalizedStringsMethods {
+  whatIsGingr: string;
+  theFirstGlobalMarket: string;
+  client: string;
+  worldwideAccessTo: string;
+  gingr: string;
+  sexWorker: string;
+  worldwideWorkWith: string;
+  agencyAndEstablishemnt: string;
+  stateOfTheArt: string;
+}
+
+export interface ReasonsToInvestStrings extends LocalizedStringsMethods {
+  reasonsToInvest: string;
+  ggCoinShould: string;
+  weProvideFirm: string;
+  ourAssetsAnd: string;
+  theGingrPlatformHas: string;
+  withYourHelp: string;
+  gingrHasAlready: string;
+  ourConceptHas: string;
+  theFirmHasGained: string;
+  weAreRegarded: string;
+  gingrIsThePioneer: string;
+}
+
+export interface HookUpStrings extends LocalizedStringsMethods {
+  hookUp: string;
+  booking: string;
+  findYourMostDesired: string;
+  clientPhone: string;
+  gingrPhone: string;
+}
