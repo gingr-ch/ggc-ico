9da143e3a404163c02074a2e642a9aabd240f45a
Unknown <jeffstahlin@gmail.com>
2018-07-27 19:41:49 +0200

    add nav links to each section

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -9,9 +9,9 @@ class Nav extends React.Component<any, any> {
   render() {
     const items = [
       { id: 1, name: this.props.lang.download, anchor: 'download' },
-      { id: 2, name: this.props.lang.platform, anchor: 'platform' },
-      { id: 3, name: this.props.lang.ggCoin, anchor: 'ggcoin' },
-      { id: 4, name: this.props.lang.icoDetails, anchor: 'icodetails' },
+      { id: 2, name: this.props.lang.platform, anchor: 'what-is-gingr' },
+      { id: 3, name: this.props.lang.ggCoin, anchor: 'blockchain' },
+      { id: 4, name: this.props.lang.icoDetails, anchor: 'ico-details' },
       { id: 5, name: this.props.lang.roadmap, anchor: 'roadmap' },
       { id: 6, name: this.props.lang.team, anchor: 'team' },
       { id: 7, name: this.props.lang.contact, anchor: 'contact' },
