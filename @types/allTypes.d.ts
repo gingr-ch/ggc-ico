9a76c47e0039f4d6de4a0e1f216935f13e939968
Unknown <jeffstahlin@gmail.com>
2018-08-08 13:34:50 +0200

    d3 textwrap

--- @types/allTypes.d.ts
+++ @types/allTypes.d.ts
@@ -3,3 +3,5 @@ declare module 'classnames';
 declare module 'nuka-carousel';
 declare module 'react-lazyload';
 declare module 'react-select-search';
+declare module 'd3';
+declare module 'd3-textwrap';
