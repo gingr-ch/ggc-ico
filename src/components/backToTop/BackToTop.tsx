c1ac90afc975fcfe6057e3e76fb97a8c4a5d2ef8
Jeff <jeffstahlin@gmail.com>
2018-08-13 21:21:23 +0200

    change backToTop to stateless component

--- src/components/backToTop/BackToTop.tsx
+++ src/components/backToTop/BackToTop.tsx
@@ -7,39 +7,37 @@ import './BackToTop.css';
 
 // const telegram = require(process.env.REACT_APP_MEDIA_URL + 'social/telegram.png');
 
-class BackToTop extends React.Component {
-  render() {
-    return (
-      <div className="btt-btn">
-        <Button click={goToTop}>
-          <svg className="arrow-img" style={{ height: '47px', width: '39px' }}>
-            <svg viewBox="0 0 45.5 44.4">
-              <polyline
-                fill="none"
-                stroke="white"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeMiterlimit="10"
-                points="15.9,25.5 22.7,18.9 29.5,25.5"
-              />
-              <circle
-                fill="none"
-                stroke="white"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeMiterlimit="10"
-                cx="22.7"
-                cy="22.2"
-                r="18.3"
-              />
-            </svg>
+export const BackToTop = () => {
+  return (
+    <div className="btt-btn">
+      <Button click={goToTop}>
+        <svg className="arrow-img" style={{ height: '47px', width: '39px' }}>
+          <svg viewBox="0 0 45.5 44.4">
+            <polyline
+              fill="none"
+              stroke="white"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeMiterlimit="10"
+              points="15.9,25.5 22.7,18.9 29.5,25.5"
+            />
+            <circle
+              fill="none"
+              stroke="white"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeMiterlimit="10"
+              cx="22.7"
+              cy="22.2"
+              r="18.3"
+            />
           </svg>
-        </Button>
-      </div>
-    );
-  }
-}
+        </svg>
+      </Button>
+    </div>
+  );
+};
 
 export default BackToTop;
