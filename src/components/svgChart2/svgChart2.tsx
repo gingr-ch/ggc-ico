b36d13021b155510241b40f4a241d42aff7a66cb
Unknown <jeffstahlin@gmail.com>
2018-08-21 15:55:21 +0200

    ICO Details 2 chart sorted with translated text

---
+++ src/components/svgChart2/svgChart2.tsx
@@ -0,0 +1,264 @@
+import * as React from 'react';
+// import './svgChartAlt.css';
+
+const Chart2 = (props: any) => (
+  <svg
+    width="100%"
+    height="468px"
+    version="1.1"
+    viewBox="0 0 271.14 247.8"
+    {...props}
+  >
+    <style
+      type="text/css"
+      dangerouslySetInnerHTML={{
+        __html:
+          '\n    .st0{fill:#4C306A;} .st1{fill-rule:evenodd;clip-rule:evenodd;fill:none;stroke:#C333FF;stroke-width:2;stroke-miterlimit:10;} .st2{fill-rule:evenodd;clip-rule:evenodd;fill:none;stroke:#D3007E;stroke-width:2;stroke-miterlimit:10;} .st3{fill:#FFFFFF;} .st4{font-family:"brandon_grotesque_regularRg"; text-transform: uppercase;} .st5{font-size:9.4466px;} .st6{font-size:8.6594px;} .st7{fill:#F3D551;} .st8{fill:none;stroke:#F3D551;stroke-miterlimit:10;} .st9{fill-rule:evenodd;clip-rule:evenodd;fill:none;stroke:#F3D551;stroke-width:2;stroke-miterlimit:10;} .st10{fill-rule:evenodd;clip-rule:evenodd;fill:none;stroke:#D3007E;stroke-miterlimit:10;} .st11{font-size:10px;} .st12{fill-rule:evenodd;clip-rule:evenodd;fill:none;stroke:#4EC441;stroke-miterlimit:10;} .st13{fill-rule:evenodd;clip-rule:evenodd;fill:#4EC441;} .st14{fill-rule:evenodd;clip-rule:evenodd;fill:#C333FF;} .st15{fill:none;stroke:#4EC441;stroke-width:2;stroke-miterlimit:10;} .st16{fill-rule:evenodd;clip-rule:evenodd;fill:#D3007E;}\n  ',
+      }}
+    />
+    <line
+      className="st1"
+      x1="254.09"
+      x2="259.29"
+      y1="42.083"
+      y2="4.083"
+      clipRule="evenodd"
+      fill="none"
+      stroke="#c333ff"
+      strokeMiterlimit={10}
+      strokeWidth={2}
+    />
+    <line
+      className="st2"
+      x1="190.09"
+      x2="236.19"
+      y1="173.78"
+      y2="117.28"
+      clipRule="evenodd"
+      fill="none"
+      stroke="#d3007e"
+      strokeMiterlimit={10}
+      strokeWidth={2}
+    />
+    <text
+      className="st3 st4 st5"
+      transform="translate(-82.512 -104.92) translate(297.25 327.91)"
+      fill="#fff"
+      fontFamily="brandon_grotesque_regularRg"
+      fontSize="9.4466px"
+    >
+      {props.lang.ggcSold}
+    </text>
+    <text
+      transform="translate(-82.512 -104.92) rotate(90 7.7854 112.96)"
+      fill="#fff"
+      fontFamily="brandon_grotesque_regularRg"
+    >
+      <tspan className="st3 st4 st5" x={0} y={0} fontSize="9.4466px">
+        EUR
+      </tspan>
+      <tspan className="st3 st4 st6" x="20.700001" y={0} fontSize="8.6594px">
+        {' '}
+      </tspan>
+      <tspan className="st3 st4 st5" x="22.6" y={0} fontSize="9.4466px">
+        / GGC
+      </tspan>
+    </text>
+    <g
+      fill="#f3d551"
+      fontFamily="brandon_grotesque_regularRg"
+      fontSize="8.6594px"
+    >
+      <text className="st7 st4 st6" x="-0.57081008" y="156.93759">
+        0.04
+      </text>
+      <text className="st7 st4 st6" x="16.80619" y="233.0119">
+        0
+      </text>
+      <text className="st7 st4 st6" x="-0.12061006" y="119.4201">
+        0.06
+      </text>
+      <text className="st7 st4 st6" x="-0.38911006" y="81.375099">
+        0.08
+      </text>
+      <text className="st7 st4 st6" x="1.5942899" y="43.596798">
+        0.10
+      </text>
+      <text className="st7 st4 st6" x="2.5727899" y="6.4268985">
+        0.12
+      </text>
+      <text className="st7 st4 st6" x="0.07908994" y="194.7619">
+        0.02
+      </text>
+    </g>
+    <g fill="none" stroke="#f3d551" strokeMiterlimit={10}>
+      <line className="st8" x1="22.588" x2="30.188" y1="4.183" y2="4.183" />
+      <line className="st8" x1="22.588" x2="30.188" y1="41.883" y2="41.883" />
+      <line className="st8" x1="22.588" x2="30.188" y1="79.483" y2="79.483" />
+      <line className="st8" x1="22.588" x2="30.188" y1="117.18" y2="117.18" />
+      <line className="st8" x1="22.588" x2="30.188" y1="154.88" y2="154.88" />
+      <line className="st8" x1="22.588" x2="30.188" y1="192.48" y2="192.48" />
+      <line className="st8" x1="22.588" x2="30.188" y1="230.18" y2="230.18" />
+      <line className="st8" x1="76.288" x2="76.288" y1="229.58" y2="237.78" />
+      <line className="st8" x1="122.09" x2="122.09" y1="229.58" y2="237.78" />
+      <line className="st8" x1="167.89" x2="167.89" y1="229.58" y2="237.78" />
+      <line className="st8" x1="213.59" x2="213.59" y1="229.58" y2="237.78" />
+      <line className="st8" x1="259.39" x2="259.39" y1="229.58" y2="237.78" />
+      <line className="st8" x1="29.988" x2="29.988" y1="229.58" y2="237.78" />
+      <polyline
+        className="st9"
+        transform="translate(-82.512 -104.92)"
+        points="342.4 334.5 113 334.5 113 108"
+        clipRule="evenodd"
+        strokeWidth={2}
+      />
+    </g>
+    <g
+      fill="#f3d551"
+      fontFamily="brandon_grotesque_regularRg"
+      fontSize="8.6594px"
+    >
+      <text className="st7 st4 st6" x="67.541489" y="247.6769">
+        200
+      </text>
+      <text className="st7 st4 st6" x="112.96439" y="247.6769">
+        400
+      </text>
+      <text className="st7 st4 st6" x="159.33688" y="247.6769">
+        600
+      </text>
+      <text className="st7 st4 st6" x="204.77249" y="247.6769">
+        800
+      </text>
+      <text className="st7 st4 st6" x="249.66359" y="247.6769">
+        1000
+      </text>
+    </g>
+    <g stroke="#f3d551" fill="none" strokeMiterlimit={10}>
+      <line className="st8" x1="53.188" x2="53.188" y1="229.58" y2="233.68" />
+      <line className="st8" x1="98.888" x2="98.888" y1="229.58" y2="233.68" />
+      <line className="st8" x1="144.69" x2="144.69" y1="229.58" y2="233.68" />
+      <line className="st8" x1="190.39" x2="190.39" y1="229.58" y2="233.68" />
+      <line className="st8" x1="236.19" x2="236.19" y1="229.58" y2="233.68" />
+    </g>
+    <line
+      className="st8"
+      x1="105.1"
+      x2="112.7"
+      y1="108.5"
+      y2="108.5"
+      fill="none"
+      stroke="#f3d551"
+      strokeMiterlimit={10}
+      transform="translate(-82.512 -104.92)"
+    />
+    <text
+      className="st3 st4 st11"
+      transform="translate(-82.512 -104.92) translate(242.48 272.13)"
+      fill="#fff"
+      fontFamily="brandon_grotesque_regularRg"
+      fontSize="10px"
+    >
+      0.03 €
+    </text>
+    <text
+      className="st3 st4 st11"
+      transform="translate(-82.512 -104.92) translate(280.33 225.06)"
+      fill="#fff"
+      fontFamily="brandon_grotesque_regularRg"
+      fontSize="10px"
+    >
+      0.06 €
+    </text>
+    <text
+      className="st3 st4 st11"
+      transform="translate(-82.512 -104.92) translate(293.2 188.23)"
+      fill="#fff"
+      fontFamily="brandon_grotesque_regularRg"
+      fontSize="10px"
+    >
+      0.08 €
+    </text>
+    <text
+      className="st3 st4 st11"
+      transform="translate(-82.512 -104.92) translate(300.87 151.06)"
+      fill="#fff"
+      fontFamily="brandon_grotesque_regularRg"
+      fontSize="10px"
+    >
+      0.10 €
+    </text>
+    <text
+      className="st3 st4 st11"
+      transform="translate(-82.512 -104.92) translate(306.87 112.56)"
+      fill="#fff"
+      fontFamily="brandon_grotesque_regularRg"
+      fontSize="10px"
+    >
+      0.12 €
+    </text>
+    <g clipRule="evenodd">
+      <path
+        className="st13"
+        d="m193.69 173.58c0 1.8-1.4 3.2-3.2 3.2s-3.2-1.4-3.2-3.2 1.4-3.2 3.2-3.2c1.7 0 3.2 1.4 3.2 3.2z"
+        fill="#4ec441"
+        fillRule="evenodd"
+      />
+      <path
+        className="st14"
+        d="m250.99 79.483c0 1.8-1.4 3.2-3.2 3.2s-3.2-1.4-3.2-3.2 1.4-3.2 3.2-3.2 3.2 1.4 3.2 3.2z"
+        fill="#c333ff"
+        fillRule="evenodd"
+      />
+    </g>
+    <g fill="none" strokeMiterlimit={10}>
+      <g stroke="#f3d551">
+        <line className="st8" x1="30.188" x2="26.088" y1="211.28" y2="211.28" />
+        <line className="st8" x1="29.888" x2="25.788" y1="173.68" y2="173.68" />
+        <line className="st8" x1="29.688" x2="25.588" y1="135.98" y2="135.98" />
+        <line className="st8" x1="29.488" x2="25.488" y1="98.283" y2="98.283" />
+        <line className="st8" x1="29.688" x2="25.588" y1="60.683" y2="60.683" />
+        <line className="st8" x1="29.588" x2="25.488" y1="23.083" y2="23.083" />
+      </g>
+      <line
+        className="st15"
+        x1="31.488"
+        x2="191.49"
+        y1="173.78"
+        y2="173.78"
+        stroke="#4ec441"
+        strokeWidth={2}
+      />
+    </g>
+    <g clipRule="evenodd">
+      <polyline
+        className="st1"
+        transform="translate(-82.512 -104.92)"
+        points="318.7 222.2 330.3 184.4 336.5 146.9"
+        fill="none"
+        stroke="#c333ff"
+        strokeMiterlimit={10}
+        strokeWidth={2}
+      />
+      <g fillRule="evenodd">
+        <path
+          className="st14"
+          d="m257.29 42.083c0 1.8-1.4 3.2-3.2 3.2s-3.2-1.4-3.2-3.2 1.4-3.2 3.2-3.2c1.7-0.1 3.2 1.4 3.2 3.2z"
+          fill="#c333ff"
+        />
+        <path
+          className="st14"
+          d="m262.59 4.183c-0.1 1.8-1.6 3.2-3.3 3.1-1.8-0.1-3.2-1.6-3.1-3.3 0.1-1.8 1.6-3.2 3.3-3.1 1.7 0 3.1 1.5 3.1 3.3z"
+          fill="#c333ff"
+        />
+        <path
+          className="st16"
+          d="m239.39 117.28c0 1.8-1.4 3.2-3.2 3.2s-3.2-1.4-3.2-3.2 1.4-3.2 3.2-3.2 3.2 1.5 3.2 3.2z"
+          fill="#d3007e"
+        />
+      </g>
+    </g>
+  </svg>
+);
+
+export default Chart2;
