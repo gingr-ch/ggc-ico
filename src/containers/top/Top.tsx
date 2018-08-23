0f46a677a60c37ba0a58ffbe37522d6b2b362a07
Unknown <jeffstahlin@gmail.com>
2018-08-23 16:32:00 +0200

    add video close button on Top component video

--- src/containers/top/Top.tsx
+++ src/containers/top/Top.tsx
@@ -27,6 +27,7 @@ class Top extends React.Component<any, any> {
     };
 
     this.videoToggle = this.videoToggle.bind(this);
+    this.close = this.close.bind(this);
     this.click = this.click.bind(this);
   }
 
@@ -39,6 +40,16 @@ class Top extends React.Component<any, any> {
       playing: true,
     });
     this.player.seekTo(0);
+  }
+
+  close() {
+    this.setState({
+      open: false,
+      muted: true,
+      volume: 0,
+      controls: false,
+      playing: true,
+    });
   }
 
   ref = (player: any) => {
@@ -61,6 +72,11 @@ class Top extends React.Component<any, any> {
     let classes = classNames({
       'gg-top-left': true,
       'gg-frame-open': this.state.open,
+    });
+
+    let btnClose = classNames({
+      'close-button__image': true,
+      'close-button__image--show': this.state.open,
     });
 
     return (
@@ -100,6 +116,26 @@ class Top extends React.Component<any, any> {
           }}
           className="player-container"
         >
+          <div className="btn-close-video">
+            <svg
+              version="1.1"
+              className={btnClose}
+              x="0px"
+              y="0px"
+              viewBox="0 0 51.976 51.976"
+              onClick={this.close}
+            >
+              <g>
+                <path
+                  d="M44.373,7.603c-10.137-10.137-26.632-10.138-36.77,0c-10.138,10.138-10.137,26.632,0,36.77s26.632,10.138,36.77,0
+                C54.51,34.235,54.51,17.74,44.373,7.603z M36.241,36.241c-0.781,0.781-2.047,0.781-2.828,0l-7.425-7.425l-7.778,7.778
+                c-0.781,0.781-2.047,0.781-2.828,0c-0.781-0.781-0.781-2.047,0-2.828l7.778-7.778l-7.425-7.425c-0.781-0.781-0.781-2.048,0-2.828
+                c0.781-0.781,2.047-0.781,2.828,0l7.425,7.425l7.071-7.071c0.781-0.781,2.047-0.781,2.828,0c0.781,0.781,0.781,2.047,0,2.828
+                l-7.071,7.071l7.425,7.425C37.022,34.194,37.022,35.46,36.241,36.241z"
+                />
+              </g>
+            </svg>
+          </div>
           <ReactPlayer
             url={video}
             width="100%"
