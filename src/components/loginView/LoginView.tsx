1dfc6c1e23a228ef5fbe9bc396358e74cceedbe5
Jeff <jeffstahlin@gmail.com>
2018-08-29 04:03:52 +0200

    add design for desktop and login adjustments

--- src/components/loginView/LoginView.tsx
+++ src/components/loginView/LoginView.tsx
@@ -1,15 +1,67 @@
 import * as React from 'react';
+import styled from 'styled-components';
+import Input from '@material-ui/core/Input';
+// import { withStyles } from '@material-ui/core/styles';
+
+// import Button from '../button/Button';
+import './LoginView.css';
+
+const LoginTitle = styled.h1`
+  text-transform: uppercase;
+  color: var(--yellow);
+  font-family: Asap;
+  font-size: 28px;
+  font-weight: 500;
+`;
+
+const Button = styled.button`
+  background: var(--yellow);
+  color: var(--purple);
+  margin-top: 2em;
+  padding: 12px 44px;
+  text-transform: uppercase;
+  border: none;
+`;
+
+// const styles = theme => ({
+//   input: {
+//     color: 'white',
+//     width: '100%',
+//     maxWidth: '311px',
+//     marginTop: '3em',
+//     '&:before': {
+//       borderBottom: '1px solid rgba(255, 255, 255, 0.42)',
+//     },
+//     '&:after': {
+//       borderBottom: '2px solid var(--yellow)',
+//     },
+//     '&:hover:before': {
+//       borderBottom: '2px solid rgba(255, 255, 255, 0.87) !important',
+//     },
+//   },
+// });
 
 const LoginView = ({ onSubmit }) => {
+  // const { classes } = props;
   return (
     <div>
-      <h1>Sign up</h1>
-      <form onSubmit={onSubmit}>
-        <label>
-          Password
-          <input name="password" type="password" placeholder="Password" />
-        </label>
-        <button type="submit">Login</button>
+      <LoginTitle>Password Protected</LoginTitle>
+      <form
+        onSubmit={onSubmit}
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Input
+          name="password"
+          type="password"
+          placeholder="Password"
+          className="login-input"
+        />
+        <Button type="submit">ENTER</Button>
       </form>
     </div>
   );
--- src/containers/login/Login.tsx
+++ src/containers/login/Login.tsx
@@ -1,8 +1,63 @@
 import * as React from 'react';
 import { withRouter } from 'react-router';
+import styled from 'styled-components';
+import Img from 'react-image';
+import ReactCSSTransitionReplace from 'react-css-transition-replace';
 import Base from '../../Base';
 
 import LoginView from '../../components/loginView/LoginView';
+
+// Require images
+const reasonsImg = require(process.env.REACT_APP_MEDIA_URL +
+  'reason_to_invest_316.png');
+const reasonsImgThumb = require(process.env.REACT_APP_MEDIA_URL +
+  'thumbs/reason_to_invest.png');
+
+// styled components
+const LoginContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+`;
+
+const LoginLeft = styled.div`
+  width: 50vw;
+  height: 100vh;
+  position: absolute;
+  left: 0;
+  top: 0;
+  background: var(--pink);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transform: translateX(-5vw);
+  z-index: 2;
+
+  &::after {
+    content: '';
+    width: 0;
+    height: 0;
+    border-style: solid;
+    border-width: 100vh 0 0 10vw;
+    border-color: transparent transparent transparent var(--pink);
+    position: absolute;
+    right: -10vw;
+    top: 0;
+    z-index: 3;
+  }
+`;
+
+const LoginRight = styled.div`
+  width: 60vw;
+  height: 100vh;
+  position: absolute;
+  right: 0;
+  top: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #78006f;
+  z-index: 1;
+`;
 
 class Login extends React.Component<any, any> {
   constructor(props: object) {
@@ -26,7 +81,48 @@ class Login extends React.Component<any, any> {
   };
 
   render() {
-    return <LoginView onSubmit={this.handleSignUp} />;
+    return (
+      <LoginContainer>
+        <LoginLeft>
+          <Img
+            src={reasonsImg}
+            style={{
+              width: 'auto',
+              height: '100%',
+              maxWidth: '316px',
+              maxHeight: '600px',
+            }}
+            alt="GGC ICO logo in a cloud"
+            className="rti-image"
+            loader={
+              <Img
+                src={reasonsImgThumb}
+                style={{
+                  width: 'auto',
+                  height: '100%',
+                  maxWidth: '316px',
+                  maxHeight: '600px',
+                  filter: 'blur(10px)',
+                }}
+              />
+            }
+            container={children => {
+              return (
+                <ReactCSSTransitionReplace
+                  transitionName="fade-wait"
+                  changeWidth={false}
+                >
+                  {children}
+                </ReactCSSTransitionReplace>
+              );
+            }}
+          />
+        </LoginLeft>
+        <LoginRight>
+          <LoginView onSubmit={this.handleSignUp} />
+        </LoginRight>
+      </LoginContainer>
+    );
   }
 }
 
