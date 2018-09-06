1a64b426aa4155a1f829baa655990f914e6425bb
Unknown <jeffstahlin@gmail.com>
2018-09-06 16:00:40 +0200

    [GGC-279] button only shows after user has scrolled x amount

--- src/components/backToTop/BackToTop.tsx
+++ src/components/backToTop/BackToTop.tsx
@@ -7,37 +7,67 @@ import './BackToTop.css';
 
 // const telegram = require(process.env.REACT_APP_MEDIA_URL + 'social/telegram.png');
 
-export const BackToTop = () => {
-  return (
-    <div className="btt-btn">
-      <Button click={goToTop}>
-        <svg className="arrow-img" style={{ height: '47px', width: '39px' }}>
-          <svg viewBox="0 0 45.5 44.4">
-            <polyline
-              fill="none"
-              stroke="white"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeMiterlimit="10"
-              points="15.9,25.5 22.7,18.9 29.5,25.5"
-            />
-            <circle
-              fill="none"
-              stroke="white"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeMiterlimit="10"
-              cx="22.7"
-              cy="22.2"
-              r="18.3"
-            />
+class BackToTop extends React.Component<any, any> {
+  prev: any;
+
+  constructor(props: object) {
+    super(props);
+    this.state = { isHide: true };
+    this.hideButton = this.hideButton.bind(this);
+  }
+
+  hideButton = () => {
+    let { isHide } = this.state;
+    const check =
+      window.scrollY > 1500
+        ? isHide && this.setState({ isHide: false })
+        : !isHide && this.setState({ isHide: true });
+
+    return check;
+  };
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.hideButton);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.hideButton);
+  }
+
+  render() {
+    let classHide = this.state.isHide ? 'hidden' : '';
+
+    return (
+      <div className={'btt-btn ' + classHide}>
+        <Button click={goToTop}>
+          <svg className="arrow-img" style={{ height: '47px', width: '39px' }}>
+            <svg viewBox="0 0 45.5 44.4">
+              <polyline
+                fill="none"
+                stroke="white"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeMiterlimit="10"
+                points="15.9,25.5 22.7,18.9 29.5,25.5"
+              />
+              <circle
+                fill="none"
+                stroke="white"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeMiterlimit="10"
+                cx="22.7"
+                cy="22.2"
+                r="18.3"
+              />
+            </svg>
           </svg>
-        </svg>
-      </Button>
-    </div>
-  );
-};
+        </Button>
+      </div>
+    );
+  }
+}
 
 export default BackToTop;
