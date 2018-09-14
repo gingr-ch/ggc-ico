e5b3d3df98b4aa42d2da6ec34e65dafc43d9b2d2
Unknown <jeffstahlin@gmail.com>
2018-09-14 12:47:57 +0200

    [GGC-374] fix table for small mobile devices

--- src/containers/icoDetails2/IcoDetails2.tsx
+++ src/containers/icoDetails2/IcoDetails2.tsx
@@ -3,7 +3,7 @@ import OnVisible from 'react-on-visible';
 
 import './IcoDetails2.css';
 
-import Arrows from '../../components/svgArrows/svgArrows';
+// import Arrows from '../../components/svgArrows/svgArrows';
 
 class IcoDetails2 extends React.Component<any, any> {
   constructor(props: any) {
@@ -14,11 +14,6 @@ class IcoDetails2 extends React.Component<any, any> {
     return (
       <div className="gg-section-container gg-section-container--ico-details2">
         <div className="gg-content-container">
-          <OnVisible className="hidden-content hidden-content--arrows">
-            <div className="table-arrow">
-              <Arrows />
-            </div>
-          </OnVisible>
           <OnVisible className="hidden-content hidden-content--flip-in-right hidden-content--ico-details2">
             <table className="table-private-sale">
               <tbody>
@@ -27,7 +22,7 @@ class IcoDetails2 extends React.Component<any, any> {
                     {this.props.lang.privateSale}
                   </td>
                   <td />
-                  <td>750</td>
+                  <td className="table-private-sale__coins">750</td>
                   <td>{this.props.lang.millionCoinsOffered}</td>
                   <td className="color--green">-75%</td>
                   <td className="color--green">€ 0.03</td>
@@ -38,7 +33,7 @@ class IcoDetails2 extends React.Component<any, any> {
                     {this.props.lang.publicPreSale}
                   </td>
                   <td />
-                  <td>150</td>
+                  <td className="table-private-sale__coins">150</td>
                   <td>{this.props.lang.millionCoinsOffered}</td>
                   <td className="color--red">-50%</td>
                   <td className="color--red">€ 0.06</td>
@@ -49,7 +44,7 @@ class IcoDetails2 extends React.Component<any, any> {
                     {this.props.lang.publicMainSale}
                   </td>
                   <td>{this.props.lang.tier} 1</td>
-                  <td>50</td>
+                  <td className="table-private-sale__coins">50</td>
                   <td>{this.props.lang.millionCoinsOffered}</td>
                   <td className="color--red">-33%</td>
                   <td className="color--purple">€ 0.08</td>
@@ -59,7 +54,7 @@ class IcoDetails2 extends React.Component<any, any> {
                     {this.props.lang.publicMainSale}
                   </td>
                   <td>{this.props.lang.tier} 2</td>
-                  <td>30</td>
+                  <td className="table-private-sale__coins">30</td>
                   <td>{this.props.lang.millionCoinsOffered}</td>
                   <td className="color--red">-16.6%</td>
                   <td className="color--purple">€ 0.10</td>
@@ -69,7 +64,7 @@ class IcoDetails2 extends React.Component<any, any> {
                     {this.props.lang.publicMainSale}
                   </td>
                   <td>{this.props.lang.tier} 3</td>
-                  <td>20</td>
+                  <td className="table-private-sale__coins">20</td>
                   <td>{this.props.lang.millionCoinsOffered}</td>
                   <td />
                   <td className="color--purple">€ 0.12</td>
