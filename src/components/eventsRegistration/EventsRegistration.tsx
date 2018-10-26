68eed6d676d9bc85a1f1afd407299e3cad7dd7e2
Jeff Stählin <jeffstahlin@gmail.com>
2018-10-26 16:09:55 +0200

    [GGC-397] add Event Registration page with working form

--- src/components/eventsRegistration/EventsRegistration.tsx
+++ src/components/eventsRegistration/EventsRegistration.tsx
@@ -1,12 +1,227 @@
 import * as React from 'react';
+import styled from 'styled-components';
+import Input from '@material-ui/core/Input';
+// import TextField from '@material-ui/core/TextField';
+import history from '../../components/BrowserHistory';
+import Button from '../../components/button/Button';
+import './EventsRegistration.css';
 
-const EventsRegistration = props => {
-  return (
-    <div>
-      <h2>Register GG Coin Meetup</h2>
-      Events register
-    </div>
-  );
+const encode = data => {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+    .join('&');
 };
+
+const Heading = styled.h2`
+  font-size: 28px;
+  color: var(--yellow);
+  text-transform: uppercase;
+  font-family: Asap;
+  font-weight: normal;
+  margin-bottom: 0em;
+`;
+
+const Container = styled.div`
+  width: 50%;
+  height: auto;
+  transform: translate3d(10%, 0, 0);
+
+  button {
+    margin-top: 1em;
+  }
+`;
+
+class EventsRegistration extends React.Component<any, any> {
+  constructor(props: object) {
+    super(props);
+
+    this.state = {
+      company: '',
+      surname: '',
+      name: '',
+      phone: '',
+      email: '',
+      sending: false,
+      sent: false,
+    };
+  }
+
+  handleChange = e => this.setState({ [e.target.name]: e.target.value });
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.setState({ sending: true });
+
+    fetch('/meetup/registration', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({ 'form-name': 'meetup', ...this.state }),
+    })
+      .then(() =>
+        setTimeout(() => {
+          this.setState({ sent: true });
+          history.push('/meetup/confirmation');
+          // tslint:disable-next-line:align
+        }, 3000)
+      )
+      .catch(error => alert(error));
+  };
+
+  render() {
+    const { company, surname, name, phone, email, sending, sent } = this.state;
+
+    return (
+      <Container>
+        <Heading>
+          Register
+          <br /> Gingr GG Coin Meetup
+        </Heading>
+        <form
+          onSubmit={this.handleSubmit}
+          name="meetup"
+          className="form flex-column"
+          style={{ paddingLeft: 0, alignItems: 'flex-start' }}
+        >
+          <input type="hidden" name="form-name" value="meetup" />
+          <Input
+            className="form__input"
+            type="text"
+            name="company"
+            id="company"
+            placeholder="Company"
+            onChange={this.handleChange}
+            value={company}
+          />
+
+          <Input
+            required={true}
+            className="form__input"
+            type="text"
+            name="surname"
+            id="surname"
+            placeholder="Surname *"
+            onChange={this.handleChange}
+            value={surname}
+          />
+          <Input
+            required={true}
+            className="form__input"
+            type="text"
+            name="name"
+            id="name"
+            placeholder="Name *"
+            onChange={this.handleChange}
+            value={name}
+          />
+          <Input
+            required={true}
+            className="form__input"
+            type="tel"
+            name="phone"
+            id="phone"
+            placeholder="Phone *"
+            onChange={this.handleChange}
+            value={phone}
+          />
+          <Input
+            required={true}
+            className="form__input"
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Email *"
+            onChange={this.handleChange}
+            value={email}
+          />
+          <Button
+            btnStyle="secondary-fat"
+            btnType="submit"
+            className={sent && 'btn-sent'}
+          >
+            {sending &&
+              !sent && (
+                <svg
+                  width="55"
+                  height="17"
+                  viewBox="0 0 120 30"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="#fff"
+                >
+                  <circle cx="15" cy="15" r="15">
+                    <animate
+                      attributeName="r"
+                      from="15"
+                      to="15"
+                      begin="0s"
+                      dur="0.8s"
+                      values="15;9;15"
+                      calcMode="linear"
+                      repeatCount="indefinite"
+                    />
+                    <animate
+                      attributeName="fill-opacity"
+                      from="1"
+                      to="1"
+                      begin="0s"
+                      dur="0.8s"
+                      values="1;.5;1"
+                      calcMode="linear"
+                      repeatCount="indefinite"
+                    />
+                  </circle>
+                  <circle cx="60" cy="15" r="9" fill-opacity="0.3">
+                    <animate
+                      attributeName="r"
+                      from="9"
+                      to="9"
+                      begin="0s"
+                      dur="0.8s"
+                      values="9;15;9"
+                      calcMode="linear"
+                      repeatCount="indefinite"
+                    />
+                    <animate
+                      attributeName="fill-opacity"
+                      from="0.5"
+                      to="0.5"
+                      begin="0s"
+                      dur="0.8s"
+                      values=".5;1;.5"
+                      calcMode="linear"
+                      repeatCount="indefinite"
+                    />
+                  </circle>
+                  <circle cx="105" cy="15" r="15">
+                    <animate
+                      attributeName="r"
+                      from="15"
+                      to="15"
+                      begin="0s"
+                      dur="0.8s"
+                      values="15;9;15"
+                      calcMode="linear"
+                      repeatCount="indefinite"
+                    />
+                    <animate
+                      attributeName="fill-opacity"
+                      from="1"
+                      to="1"
+                      begin="0s"
+                      dur="0.8s"
+                      values="1;.5;1"
+                      calcMode="linear"
+                      repeatCount="indefinite"
+                    />
+                  </circle>
+                </svg>
+              )}
+            {sending && sent && 'Sent'}
+            {!sent && !sending && 'Send'}
+          </Button>
+        </form>
+      </Container>
+    );
+  }
+}
 
 export default EventsRegistration;
