a84e3d5ffa2394af12d092ee9e6f83fec92fb547
Jeff Stählin <jeffstahlin@gmail.com>
2018-11-13 02:01:27 +0100

    add what should be last improvements to nav

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -10,26 +10,26 @@ import './Header.css';
 const gingrLogo = require(process.env.REACT_APP_MEDIA_URL + 'logo.png');
 
 const users = {
-  'friends1': 'L247',
-  'friends2': 'L229',
-  'friends3': 'L198',
-  'friends4': 'L230',
-  'friends5': 'L224',
-  'friends6': 'L231',
-  'friends7': 'L232',
-  'friends8': 'L234',
-  'friends9': 'L235',
-  'friends10': 'L236',
-  'friends11': 'L237',
-  'friends12': 'L238',
-  'friends13': 'L239',
-  'friends14': 'L240',
-  'friends15': 'L241',
-  'friends16': 'L242',
-  'friends17': 'L243',
-  'friends18': 'L244',
-  'friends19': 'L245',
-  'friends20': 'L246'
+  friends1: 'L247',
+  friends2: 'L229',
+  friends3: 'L198',
+  friends4: 'L230',
+  friends5: 'L224',
+  friends6: 'L231',
+  friends7: 'L232',
+  friends8: 'L234',
+  friends9: 'L235',
+  friends10: 'L236',
+  friends11: 'L237',
+  friends12: 'L238',
+  friends13: 'L239',
+  friends14: 'L240',
+  friends15: 'L241',
+  friends16: 'L242',
+  friends17: 'L243',
+  friends18: 'L244',
+  friends19: 'L245',
+  friends20: 'L246',
 };
 
 let adminIco = 'https://admin.ggcico.io/auth/register?refcode=';
@@ -48,7 +48,6 @@ class Header extends React.Component<any, any> {
     this.state = {
       right: false,
     };
-
   }
 
   click(url: string) {
@@ -63,12 +62,19 @@ class Header extends React.Component<any, any> {
 
   render() {
     if (this.props.user) {
-      adminIco = 'https://admin.ggcico.io/auth/register?refcode=' + users[this.props.user];
+      adminIco =
+        'https://admin.ggcico.io/auth/register?refcode=' +
+        users[this.props.user];
     }
     return (
       <header className="gg-header">
         <div className="gg-header-container">
-          <Img src={gingrLogo} className="App-logo" alt="logo" />
+          <Img
+            src={gingrLogo}
+            className="App-logo"
+            alt="logo"
+            style={{ minWidth: '123px', minHeight: '48px' }}
+          />
           {this.props.children}
           <div className="gg-header-container__buttons">
             <a
