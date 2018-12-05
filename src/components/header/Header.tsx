808405c860d2ee4c493032527a88202b4af89c08
Jeff Stählin <jeffstahlin@gmail.com>
2018-12-05 15:46:17 +0100

    fix nav on mobile

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -74,6 +74,16 @@ class Header extends React.Component<any, any> {
         users[this.props.user];
     }
 
+    const items = [
+      { id: 1, name: this.props.lang.download, anchor: 'download' },
+      { id: 2, name: this.props.lang.platform, anchor: 'what-is-gingr' },
+      { id: 3, name: this.props.lang.ggCoin, anchor: 'blockchain' },
+      { id: 4, name: this.props.lang.icoDetails, anchor: 'ico-details' },
+      { id: 5, name: this.props.lang.roadmap, anchor: 'roadmap' },
+      { id: 6, name: this.props.lang.team, anchor: 'team' },
+      { id: 7, name: this.props.lang.contact, anchor: 'contact' },
+    ];
+
     return (
       <header className="gg-header">
         <div className="gg-header-container">
@@ -171,7 +181,7 @@ class Header extends React.Component<any, any> {
               onClick={this.toggleDrawer('right', false)}
               onKeyDown={this.toggleDrawer('right', false)}
             >
-              <Nav lang={this.props.lang} side={true} />
+              <Nav items={items} side={true} />
             </div>
           </Drawer>
         </div>
